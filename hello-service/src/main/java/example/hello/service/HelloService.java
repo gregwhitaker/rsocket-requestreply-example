ba3dd5a200738cebd8d5755098176ace00dc4098
gregwhitaker <gwhitake@gmail.com>
2020-01-16 15:24:27 -0800

    Added loggers

--- hello-service/src/main/java/example/hello/service/HelloService.java
+++ hello-service/src/main/java/example/hello/service/HelloService.java
@@ -1,6 +1,10 @@
 package example.hello.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloService {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloService.class);
 
     public static void main(String... args) {
 
