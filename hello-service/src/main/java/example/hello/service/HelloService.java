6e03c46ac7871a42494165e68b1c197ce3e5dd7c
gregwhitaker <gwhitake@gmail.com>
2020-01-16 15:28:10 -0800

    Implemented the hello-service

--- hello-service/src/main/java/example/hello/service/HelloService.java
+++ hello-service/src/main/java/example/hello/service/HelloService.java
@@ -1,12 +1,50 @@
 package example.hello.service;
 
+import io.rsocket.AbstractRSocket;
+import io.rsocket.ConnectionSetupPayload;
+import io.rsocket.Payload;
+import io.rsocket.RSocket;
+import io.rsocket.RSocketFactory;
+import io.rsocket.SocketAcceptor;
+import io.rsocket.frame.decoder.PayloadDecoder;
+import io.rsocket.transport.netty.server.TcpServerTransport;
+import io.rsocket.util.DefaultPayload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
 
+/**
+ * Service that generates a hello message.
+ */
 public class HelloService {
     private static final Logger LOG = LoggerFactory.getLogger(HelloService.class);
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
+        RSocketFactory.receive()
+                .frameDecoder(PayloadDecoder.DEFAULT)
+                .acceptor(new SocketAcceptor() {
+                    @Override
+                    public Mono<RSocket> accept(ConnectionSetupPayload setup, RSocket sendingSocket) {
+                        return Mono.just(new AbstractRSocket() {
+                            @Override
+                            public Mono<Payload> requestResponse(Payload payload) {
+                                String name = payload.getDataUtf8();
 
+                                if (name == null || name.isEmpty()) {
+                                    name = "You";
+                                }
+
+                                return Mono.just(DefaultPayload.create(String.format("Hello, %s!", name)));
+                            }
+                        });
+                    }
+                })
+                .transport(TcpServerTransport.create(7000))
+                .start()
+                .block();
+
+        LOG.info("RSocket server started on port: 7000");
+
+        Thread.currentThread().join();
     }
 }
