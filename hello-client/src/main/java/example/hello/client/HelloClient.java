ed17761b82c56d44a631180084c090cdca30aa4c
gregwhitaker <gwhitake@gmail.com>
2020-01-16 15:23:05 -0800

    Getting the service and client stubs in place

---
+++ hello-client/src/main/java/example/hello/client/HelloClient.java
@@ -0,0 +1,8 @@
+package example.hello.client;
+
+public class HelloClient {
+
+    public static void main(String... args) {
+        
+    }
+}
---
+++ hello-service/src/main/java/example/hello/service/HelloService.java
@@ -0,0 +1,8 @@
+package example.hello.service;
+
+public class HelloService {
+
+    public static void main(String... args) {
+
+    }
+}
